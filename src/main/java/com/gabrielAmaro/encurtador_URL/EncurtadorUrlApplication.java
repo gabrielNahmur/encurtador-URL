47807b10f07ea0421e86d94207ebb2b53e33d90d
Gabriel Amarom <gabrielnahmur@gmail.com>
2025-07-03 17:37:48 -0300

    chore: Add main application class and Redis configuration

---
+++ src/main/java/com/gabrielAmaro/encurtador_URL/EncurtadorUrlApplication.java
@@ -0,0 +1,12 @@
+package com.gabrielAmaro.encurtador_URL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorUrlApplication.class, args);
+	}
+}
